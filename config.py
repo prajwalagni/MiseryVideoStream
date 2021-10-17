c603e4f11119390f57e01daf140cdef3a70b374d
prajwalagni <59473567+prajwalagni@users.noreply.github.com>
2021-10-17 20:17:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ if path.exists("local.env"):
 
 load_dotenv()
 admins = {}
-BOT_TOKEN = getenv("BOT_TOKEN", None)
-API_ID = int(getenv("API_ID", "6"))
-API_HASH = getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-SESSION_NAME = getenv("SESSION_NAME", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
+SESSION_NAME = getenv("SESSION_NAME", "")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "50"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "MiseryVideoStreamer")
